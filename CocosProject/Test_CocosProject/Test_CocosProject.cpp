48c95b5db97e5ce6350fefa9720947568906d72a
ricardo <klossricardo@gmail.com>
2015-04-29 22:46:54 -0300

    After some voodoo the gtest now can work with cocos functions.
    Signed Off By <ricardo - rbk@dcc.ufmg.br>

--- CocosProject/Test_CocosProject/Test_CocosProject.cpp
+++ CocosProject/Test_CocosProject/Test_CocosProject.cpp
@@ -3,6 +3,18 @@
 
 #include "gtest\gtest.h"
 
+#include "cocos2d.h"
+
+#if _MSC_VER > 1700
+#pragma comment(lib,"libcocos2d_2013.lib")
+#pragma comment(lib,"libbox2d_2013.lib")
+#pragma comment(lib,"libSpine_2013.lib")
+#else
+#pragma comment(lib,"libcocos2d_2012.lib")
+#pragma comment(lib,"libbox2d_2012.lib")
+#pragma comment(lib,"libSpine_2012.lib")
+#endif
+
 TEST(SumTest, TwoTwo) {
 	EXPECT_EQ(4, 2 + 2);
 }
